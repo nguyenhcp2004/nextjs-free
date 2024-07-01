f51fa2fc9b7da077aa1981a8c64c43fcf67f916b
Huynh Nguyen <yugivip277@gmail.com>
2024-07-01 14:08:15 +0700

    Code trang Login

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -1,3 +1,12 @@
+import LoginForm from "@/app/(auth)/login/login-form";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <h1 className="text-xl font-semibold text-center">Đăng ký</h1>
+      <div className="flex justify-center">
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
