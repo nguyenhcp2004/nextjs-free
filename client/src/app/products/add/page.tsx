4deea05c887d2f305c3af7bb777421b9c4afc6b6
Huynh Nguyen <yugivip277@gmail.com>
2024-07-10 11:46:27 +0700

    Upload hình ảnh và tạo sản phẩm

---
+++ client/src/app/products/add/page.tsx
@@ -0,0 +1,11 @@
+import ProductAddForm from '@/app/products/add/product-add-form'
+import React from 'react'
+
+export default function ProductAddPage() {
+  return (
+    <div>
+      <h1>ProductAddPage</h1>
+      <ProductAddForm />
+    </div>
+  )
+}
--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -8,6 +8,9 @@ export default function Header() {
     <div>
       <ul>
         <li>
+          <Link href='/products/add'>Thêm sản phẩm</Link>
+        </li>
+        <li>
           <Link href='/login'>Đăng nhập</Link>
         </li>
         <li>
